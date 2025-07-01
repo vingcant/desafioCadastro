2a0772d42c5451e9d2fc31bcdcb406f3368b7fe4
vingcant <kaikecantuario@gmail.com>
2025-06-30 23:50:45 -0300

    Desafio Cadastro

--- src/menuservices/Pet.java
+++ src/menuservices/Pet.java
@@ -9,6 +9,9 @@ public class Pet {
     Scanner input = new Scanner(System.in);
     String answer;
     boolean validation;
+    boolean numHouse;
+    boolean city;
+    boolean street;
 
     public void cadastro() {
         System.out.println("Digite a seguir as respostas de acordo com a numeração.");
@@ -42,7 +45,7 @@ public class Pet {
         } while (!validation);
 
 
-        System.out.println("Digite a letra correspondente ao sexo entre Cat e Dog.");
+        System.out.println("Digite a letra correspondente ao sexo.");
         do {
             System.out.print("3: ");
             answer = input.nextLine().toUpperCase();
@@ -55,6 +58,14 @@ public class Pet {
             }
 
         } while (!validation);
+
+        System.out.println("Escreva o endereço onde o animal foi encontrado.");
+        System.out.println("4: ");
+        do {
+            System.out.print("Número da casa: ");
+            answer = input.nextLine();
+
+        }while(!numHouse);
 
     }
 }
